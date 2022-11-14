9d74ab4418f7db5070c62dd77ad480304369cb84
Srdjan Djuric <srdjan.djuric003@gmail.com>
2022-11-14 15:35:27 +0100

    get,post,put,fetch,delete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './interface/user';
 import { UserService } from './service/user.service';
 
 @Component({
@@ -9,18 +10,31 @@ import { UserService } from './service/user.service';
 export class AppComponent implements OnInit {
   title = 'AngularHttp';
 
+  private user: any = {
+    'id': 5,
+    'name': 'Srdjan Djuric',
+    'username': 'Srx',
+    'email': 'srx@gmail.com',
+    
+    };
+
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+      this.patchUser();
       this.getUsers();
-      this.getUser();
+      // this.getUser();
+      // this.addUser();
+      // this.updateUser();
+    
+
   }
 
   getUsers(): void {
     this.userService.getUsers().subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
-      () => console.log("done getting user"),
+      () => console.log('succesfully fetched users'),
     );
 
   }
@@ -29,10 +43,38 @@ export class AppComponent implements OnInit {
     this.userService.getUser().subscribe(
       (response) => console.log(response),
       (error) => console.log(error),
-      () => console.log("done getting user"),
+      () => console.log('succesfully fetched user'),
+    );
+
+  };
+
+  addUser(): void {
+    this.userService.addUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+      () => console.log('succesfully added user'),
     );
 
   }
+
+  updateUser(): void {
+    this.userService.updateUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+      () => console.log('succesfully updated user'),
+    );
+
+  }
+
+  patchUser(): void {
+    this.userService.patchUser(this.user).subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+      () => console.log('succesfully patched user'),
+    );
+
+  }
+
 
 
 }
