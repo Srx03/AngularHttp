905d7dceb5a6625adc11a344467e81bdfa8733b4
Srdjan Djuric <srdjan.djuric003@gmail.com>
2022-11-14 01:36:54 +0100

    subscribing to observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularHttp';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+      this.getUsers();
+      this.getUser();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+      () => console.log("done getting user"),
+    );
+
+  }
+
+  getUser(): void {
+    this.userService.getUser().subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error),
+      () => console.log("done getting user"),
+    );
+
+  }
+
+
 }
